544efaae9f55baeac267b066d74ae0ed2c2d14af
Mark Pitblado <116753126+mpitblado@users.noreply.github.com>
2025-02-12 12:23:14 -0800

    tests: convert_df_to_json

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,4 +1,4 @@
-from helpers.data import load_data
+from helpers.data import load_data, convert_df_to_json
 
 import pandas as pd
 import numpy as np
@@ -22,4 +22,20 @@ def test_skip_rows():
     pd.testing.assert_frame_equal(
         load_data(filepath="tests/data/test-data.csv", skip_blank_rows=True),
         test_df,
+    )
+
+
+def test_df_to_json():
+    test_json = {
+        "1": {"test4": "test"},
+        "2": {"test4": None},
+        "3": {"test4": "test"},
+    }
+    assert (
+        convert_df_to_json(
+            load_data(
+                filepath="tests/data/test-data.csv", skip_blank_rows=False
+            )
+        )
+        == test_json
     )
